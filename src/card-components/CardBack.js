d1ac520b36a306b51bdadbce494dc41ee170c841
Trent Moss <trentmoss05@gmail.com>
2019-05-20 17:05:42 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -11,7 +11,10 @@ const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 export default class CardBack extends Component {
 
   generateRatingElement = () => {
-    // implement meeeee! See the readme for instructions
+    if (this.props.IMDBRating !== null) {
+			return <img src={imgMapper[this.props.IMDBRating]} alt="" />;
+		}
+		return <h4>No Rating Found</h4>;
   }
 
   render() {
